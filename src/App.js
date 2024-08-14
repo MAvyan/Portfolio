c6a62203822f7b791febb9d45b0b9e94d711fac2
MAVINGAyannis <y.mavinga.pro@gmail.com>
2024-08-15 00:24:52 +0200

    page unmoved

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home/Home";
-import About from "./components/About/Page";
+import About from "./components/About/About";
 import Projects from "./components/Projects/Profil";
 import Resume from "./components/Resume/ResumeNew";
 import {
