d3b040a0a9e5e6802cadfd22041502b25516b783
MAVINGAyannis <y.mavinga.pro@gmail.com>
2024-08-17 00:33:13 +0200

    myyoutube

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -31,7 +31,7 @@ const projectData = [
         imgPath: myyoutube,
         title: "MyYoutube",
         description:
-            "J'ai réalisé l'intégration d'un clone de YouTube pour un projet d'école en utilisant React.",
+            "J'ai développé un clone de YouTube en utilisant React et Tailwind CSS pour un projet d'école. Ce projet m'a permis de recréer une interface utilisateur complexe tout en optimisant le design et les performances grâce à Tailwind. J'ai pu approfondir mes compétences en développement front-end et en structuration de composants pour des applications web modernes.",
         ghLink: "https://github.com/MAvyan/MyYoutube",
     },
     {
